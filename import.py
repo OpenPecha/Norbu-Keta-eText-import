30b583ae418002801a4625e1bdf669d2b51026e9
jungtop <jungtop@jungtops-MBP.bbrouter>
2023-03-03 15:33:19 +0530

    download obj

--- import.py
+++ import.py
@@ -45,7 +45,7 @@ def get_s3_prefix_path(
         for dt in data_types:
             paths[dt] = f"{batch_dir}/{dt}/{work_local_id}-{suffix}"
         return paths
-    return f"s3://ocr.bdrc.io/{base_dir}/norbuketaka/batch-0001"
+    return f"{base_dir}/norbuketaka2/batch-0001"
 
 def get_info_json():
     info_json = {
@@ -53,11 +53,11 @@ def get_info_json():
 }
     return info_json
 
-def archive_on_s3(s3_path,csv_file:Path):
+def archive_on_s3(s3_path,csv_file:Path,file_name):
     info_json=get_info_json()
     csv_content = csv_file.read_text(encoding="utf-8")
-    s3_ocr_info_path = f"{s3_path}/{INFO_FN}"
-    s3_ocr_csv_path = f"{s3_path}/{csv_file.stem}.csv"
+    s3_ocr_info_path = f"{s3_path}/info.json"
+    s3_ocr_csv_path = f"{s3_path}/{file_name}"
     """ ocr_output_bucket.put_object(
         Key=s3_ocr_info_path, Body=(bytes(json.dumps(info_json).encode("UTF-8")))
     )
@@ -70,7 +70,7 @@ def get_csvFiles(dir):
     return csv_files
 
 def extract_ids(str):
-    x = re.match("(.*)-(.*)",str)
+    x = re.match("(.*)-(.*)\.(csv)+",str)
     work_id = x.group(1)
     image_group_id = x.group(2)
     return work_id,image_group_id
@@ -78,8 +78,9 @@ def extract_ids(str):
 
 def main(csv_file:Path):
     work_id,image_group_id = extract_ids(csv_file.stem)
+    file_name = f"{work_id}-{image_group_id}.csv"
     s3_prefix = get_s3_prefix_path(work_id,image_group_id)
-    archive_on_s3(s3_prefix,csv_file)
+    archive_on_s3(s3_prefix,csv_file,file_name)
 
 
 def extract_meta():
@@ -96,6 +97,11 @@ def delete_obj_from_bucket(file_name):
     print(res)
 
 if __name__ == "__main__":
+    """ path = "NorbuKetaka2"
+    csv_files = get_csvFiles(path)
+    for csv_file in csv_files:
+        main(csv_file) """
+
     objects = ocr_output_bucket.objects.filter(Prefix='NorbuKetaka2/')
     for file in objects:
         S3_client.download_file(OCR_OUTPUT_BUCKET,file.key,f"{file.key}.csv")
